bba203fd687240739a1e48ed25f4ee758513d48b
Guillaume ARM <trapcodien@gmail.com>
2020-07-23 19:34:13 +0200

    feat: typescript support

---
+++ types/index.d.ts
@@ -0,0 +1,4 @@
+export declare function noteNameToNoteNumber(note: string): number | undefined;
+export declare function noteNumberToFrequency(note: number): number;
+export declare function noteNumberToName(note: number): string | undefined;
+export declare function frequencyToNoteNumber(freq: number): number;
